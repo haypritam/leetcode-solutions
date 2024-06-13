b9e99f474673fb441c798fcf0bfe5f91a82a9168
haypritam <haypritam@gmail.com>
2024-06-13 15:35:40 +0530

    2037 python easy

---
+++ easy/2037-Minimum_Number_Of_Moves_To_Seat_Everyone/minimum_number_of_moves_to_seat_everyone.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        seats.sort()
+        students.sort()
+        
+        total_moves = 0
+        n = len(seats)
+        for i in range(n):
+            total_moves += abs(students[i] - seats[i])
+        
+        return total_moves
